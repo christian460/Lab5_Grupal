1bd7299d67bf391f956d61fb2c7c431f535df5b6
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-07-23 17:16:17 -0500

    Se creo el modelo de las clases Libro y User

--- lab8/library/reservation/models.py
+++ lab8/library/reservation/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    nombre = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    codigo = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre
+
+class User(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    deuda = models.BooleanField(default=False)
+    libroPrest = models.ForeignKey(Libro, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+
+
+
