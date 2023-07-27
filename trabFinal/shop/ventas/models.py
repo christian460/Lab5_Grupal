0617275621629c94e2d4dd960e65ddca0dcdba8d
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-07-27 03:17:45 -0500

    Se implemento la app venta

--- trabFinal/shop/ventas/models.py
+++ trabFinal/shop/ventas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from user.models import Cliente
 # Create your models here.
+
+class Producto(models.Model):
+    codigo = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    img = models.ImageField(upload_to='pics', default='')
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.nombre
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha_venta = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Venta del cliente: {self.cliente.nombre} - Fecha: {self.fecha_venta}"
--- trabFinal/shop/ventas/urls.py
+++ trabFinal/shop/ventas/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name="index"),
+    path('agregar',views.agregar, name="agregar"),
+    path('modificar/<int:pro_id>/',views.modificar, name="modificar"),
+    path('listar',views.lista, name="lista"),
+    path('eliminar/<int:pro_id>/',views.eliminar, name="eliminar"),
+
 ]
